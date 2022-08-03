d8929908d9efc4677e5cfd5391080f55a13339d7
Tomasan7 <tomashula06@gmail.com>
2022-08-03 15:02:28 +0200

    Create debug object in Main

--- src/main/kotlin/me/tomasan7/tomfeedbackapi/Main.kt
+++ src/main/kotlin/me/tomasan7/tomfeedbackapi/Main.kt
@@ -1,19 +1,30 @@
 package me.tomasan7.tomfeedbackapi
 
-import me.tomasan7.tomfeedbackapi.componentbuilding.buildComponent
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextColor
-import net.kyori.adventure.text.format.TextDecoration
+import me.tomasan7.tomfeedbackapi.feedbackelement.MessageFeedback
+import net.kyori.adventure.text.minimessage.MiniMessage
 
 fun main()
 {
-    buildComponent {
-        text("You're a ")
-        color(TextColor.color(0x443344))
-        keybind("key.jump") {
-            color(NamedTextColor.LIGHT_PURPLE)
-            decoration(TextDecoration.BOLD, true)
-        }
-        text(" to jump!")
-    }
+    val feedbackYaml = mapOf<String, Any>(
+        "message" to "<red>Welcome to the server!",
+        "title" to mapOf<String, Any>(
+            "title" to "<red>Welcome to the server! (title)",
+            "sub-title" to "<dark_purple>We hope you'll like it here.",
+            "fade-in" to 20,
+            "stay" to 20,
+            "fade-out" to 20
+        ),
+        "action-bar" to "<white>Type /help for help.",
+        "sound" to mapOf<String, Any>(
+            "key" to "entity.experience_orb.pickup",
+            "source" to "AMBIENT",
+            "volume" to 1.0,
+            "pitch" to 2.0
+        )
+    )
+
+    val placeholders = Placeholders.of("%GAY%", "KINGF007")
+    val messageFeedback = MessageFeedback.deserialize(feedbackYaml["message"]!!, MiniMessage.miniMessage(), placeholders)
+
+    println(messageFeedback)
 }
