3ff2f075db2016cd000ffe6bccbeb94ac8d89b41
Tomasan7 <tomashula06@gmail.com>
2023-08-14 09:10:17 +0200

    Add proper repository for kotlin plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "feedbacklib"
+
+pluginManagement {
+    repositories {
+        // https://kotlinlang.org/docs/whatsnew19.html#configure-gradle-settings
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
